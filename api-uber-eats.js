2ec7fba4971abe98db38ba22ef10b923983f99aa
rafagomez92 <rafael_gomez92@hotmail.com>
2020-03-06 02:31:04 -0600

    Modifique mi aplicacion principal y agregue las rutas para las subaplicaciones

--- api-uber-eats.js
+++ api-uber-eats.js
@@ -1,44 +1,31 @@
 import http from 'http';
 import express from 'express';
-import dotenv from 'dotenv';
+import client from './components/client';
+import delivery from './components/delivery';
+import restaurant from './components/restaurant';
 import restaurants from './modules/restaurants';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const APP = express();
+const CLIENT = express();
+const DELIVERY = express();
+const RESTAURANT = express();
+
+const pedidos = [];
+
+APP.use('/client', CLIENT);  //Ruta exclusiva para el cliente
+APP.use('/delivery', DELIVERY);  //Ruta exclusiva para el repartidor
+APP.use('/restaurant', RESTAURANT);  //Ruta exclusiva para restaurant
 
 const SERVER = http.createServer(APP);
 
-APP.get('/restaurants/:zona', (req, res) => {
-    const zona = req.params.zona;              
-    switch (zona) {
-        case 'norte':                       
-            const norte = `Restaurantes de la zona ${zona}: ${JSON.stringify(restaurants[0].restaurantes[0].menu)}, ${JSON.stringify(restaurants[0].restaurantes[1].nombre)}`;
-            res.send(norte);            
-            break;
-        case 'sur':            
-            const sur = `Restaurantes de la zona ${zona}: ${JSON.stringify(restaurants[1].restaurantes[0].nombre)}, ${JSON.stringify(restaurants[1].restaurantes[1].nombre)}`
-            res.send(sur);
-            break;
-        case 'oriente':
-            const oriente = `Restaurantes de la zona ${zona}: ${JSON.stringify(restaurants[2].restaurantes[0].nombre)}, ${JSON.stringify(restaurants[2].restaurantes[1].nombre)}`
-            res.send(oriente);
-            break;
-        case 'poniente':
-            const poniente = `Restaurantes de la zona ${zona}: ${JSON.stringify(restaurants[3].restaurantes[0].nombre)}, ${JSON.stringify(restaurants[3].restaurantes[1].nombre)}`
-            res.send(poniente);
-            break;
-        default:
-            res.send("Ingresaste una zona incorrecta");
-    } 
+
+APP.get('/', (req, res) => {    
+    res.send('Bienvenido');    
 });
 
-APP.get('/:zona/:restaurante', (req, res) => {             
-    const menu = restaurants.filter(platillo => platillo.zona === req.params.zona);    
-    // res.send(`${JSON.stringify(restaurants[Number(id)].zona)}`);    
-    // res.send(`${JSON.stringify(restaurants[Number(id)].restaurantes[1])}`);
-    res.send(`${JSON.stringify(menu)}` );
-    
-});
+client(CLIENT, restaurants, pedidos);
 
 SERVER.listen(process.env.PORT);
