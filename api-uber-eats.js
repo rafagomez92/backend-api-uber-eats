0f69c766692482a3699996b7d42d9a63ca5fed35
rafagomez92 <rafael_gomez92@hotmail.com>
2020-03-07 10:40:28 -0600

    Se modifico la app principal y se agregaron 3 sub apps

--- api-uber-eats.js
+++ api-uber-eats.js
@@ -13,8 +13,12 @@ const CLIENT = express();
 const DELIVERY = express();
 const RESTAURANT = express();
 
-const pedidos = [];
-
+const pedidos = [];  //Array que almacena los pedidos por el usuario
+const accept = []; //Array que almacena el pedido y la variable de status
+const status = [false]; //Array que almacena una variable de status
+const deliveryOrder = []; //array que almacena los pedidos para el repartidor
+const statusDelivery = [false];
+const acceptRestaurant = [];
 APP.use('/client', CLIENT);  //Ruta exclusiva para el cliente
 APP.use('/delivery', DELIVERY);  //Ruta exclusiva para el repartidor
 APP.use('/restaurant', RESTAURANT);  //Ruta exclusiva para restaurant
@@ -26,6 +30,8 @@ APP.get('/', (req, res) => {
     res.send('Bienvenido');    
 });
 
-client(CLIENT, restaurants, pedidos);
+client(CLIENT, restaurants, pedidos, accept, status);
+restaurant(RESTAURANT, restaurants, pedidos, accept, status, deliveryOrder, statusDelivery, acceptRestaurant);
+delivery(DELIVERY, restaurants, pedidos, accept, status, deliveryOrder, statusDelivery, acceptRestaurant);
 
 SERVER.listen(process.env.PORT);
