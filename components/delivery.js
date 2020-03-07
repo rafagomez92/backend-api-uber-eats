d5f877b12c132fd493c046f43e6840f415b19129
rafagomez92 <rafael_gomez92@hotmail.com>
2020-03-07 10:41:44 -0600

    Esta sub app es todo lo relacionado con el repartidor

--- components/delivery.js
+++ components/delivery.js
@@ -1,4 +1,77 @@
+import restaurants from '../modules/restaurants';
 
+export default (DELIVERY, restaurants, pedidos, accept, status, deliveryOrder, statusDelivery, acceptRestaurant) => { 
+    let statusOrder = false;
+    let cancel = true;
+    
+    DELIVERY.get('/', (req, res) => {            
+        if (statusDelivery[0] === true) {
+            deliveryOrder[0].order = 'stand by';            
+            res.json(deliveryOrder);            
+        } else {
+            res.send('No hay pedidos');
+        }
+    });
+
+
+    //Aceptamos nuestro pedido con un query http://localhost:3000/delivery/accept?confirm=yes
+    DELIVERY.get('/accept', (req, res) => {
+        const accept = deliveryOrder.find(p => p.order === 'stand by');
+        const confirm = req.query.confirm;
+        if (confirm === 'yes') {
+            deliveryOrder[0].order = 'accept';            
+            res.json(deliveryOrder);
+            statusOrder = true;
+            cancel = false;
+        } else {
+            res.json(deliveryOrder);
+        }
+    });
+
+    
+    //Ver pedido actual
+    DELIVERY.get('/pedidos', (req, res) => {
+        if (statusOrder === true) {
+            res.json(deliveryOrder);
+        } else {
+            res.send('No hay pedidos aceptados');
+        }
+    });
+
+    //Orden finalizada
+    DELIVERY.get('/finish', (req, res) => {
+        if (statusOrder === true) {
+            deliveryOrder[0].order = 'finish';            
+            res.json(deliveryOrder);
+            deliveryOrder.length = 0;
+            statusDelivery[0] = false;            
+            statusOrder = false;            
+            accept.length = 0;
+            pedidos.length = 0;
+            acceptRestaurant.length = 0;
+            status[0] = false;
+        } else {
+            res.send('No hay pedidos');
+        }
+    });
+    
+    //cancelamos el pedido
+    DELIVERY.delete('/cancel', (req,res) => {  
+        if (cancel === false) {
+            res.send('No hay pedidos por cancelar');
+        } else {                                    
+            deliveryOrder.length = 0;
+            statusDelivery[0] = false;            
+            statusOrder = false;            
+            accept.length = 0;
+            pedidos.length = 0;
+            acceptRestaurant.length = 0;
+            status[0] = false;
+            res.send('Se borraron todos los pedidos');            
+        }
+    });
+
+}
 
 
 
