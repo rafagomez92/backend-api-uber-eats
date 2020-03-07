9e8182c683d923438b26bd6293bc17fd9c0a41f4
rafagomez92 <rafael_gomez92@hotmail.com>
2020-03-07 10:41:07 -0600

    Esta sub app contiente todo lo relacionado a la accion del cliente

--- components/client.js
+++ components/client.js
@@ -2,7 +2,7 @@ import restaurants from '../modules/restaurants';
 import { v4 as uuidv4 } from 'uuid';
 
 
-export default (CLIENT, restaurants, pedidos) => {  
+export default (CLIENT, restaurants, pedidos, accept, status) => {      
 
     //Para checar los restaurantes por zona puedes ingresar (norte, sur, oriente, poniente)
     //La zona se obtiene a traves de un query http://localhost:3000/client/zona?zona=sur
@@ -29,22 +29,26 @@ export default (CLIENT, restaurants, pedidos) => {
     //Esta es la ruta para seleccionar los platillos
     //Ingresamos en el path el nombre del restaurant y con un query obtenemos el platillo que vamos a seleccionar
     CLIENT.post('/restaurant/:name', (req, res) => {        
-        const restaurantName = restaurants.find(p => p.name === req.params.name);
-        if (restaurantName) {
-            const pedido = restaurantName.dishes.find(p => p.dish === req.query.pedido);
-            pedido.id = uuidv4();
-            if (pedido) {                
-                pedidos.push(pedido);                
+        if (status[0] === false) {
+            const restaurantName = restaurants.find(p => p.name === req.params.name);
+            if (restaurantName) {
+                const pedido = restaurantName.dishes.find(p => p.dish === req.query.pedido);
+                pedido.id = uuidv4();            
+                if (pedido) {                
+                    pedidos.push(pedido);                
+                } else {
+                    res.send('Ese platillo no lo tenemos en el menu');
+                }
             } else {
-                res.send('Ese platillo no lo tenemos en el menu');
-            }
+                res.sendStatus(404);
+            }        
+            res.json(pedidos);        
         } else {
-            res.sendStatus(404);
-        }        
-        res.json(pedidos);        
+            res.send('Ya tienes un pedido por el momento');
+        }
     });
 
-    //Esta ruta me sirve para ver los pedidos y para cancelar un platillo (a traves de un query)
+    //Ruta para cancelar un platillo (a traves de un query)
     //el pedido se elimina a traves de su id
     CLIENT.delete('/pedidos', (req,res) => {
         if (req.query.id) {
@@ -69,11 +73,34 @@ export default (CLIENT, restaurants, pedidos) => {
         }
     })
 
-    //Cancelar el pedido actual
-    CLIENT.get('/cancel', (req,res) => {
-        pedidos = [];
-        res.send('El pedido se ha cancelado');
-        console.log(pedidos);
+    //Cancelar el pedido actual con la siguiente ruta    
+    CLIENT.delete('/pedidos/cancel', (req,res) => {        
+        if (pedidos.length > 0) {
+                pedidos = [];
+                status[0] = false;
+                accept = [];
+                res.send('El pedido se ha cancelado');
+            } else {            
+                res.send('No hay ningun pedido para cancelar');
+            }
+        
+    });
+
+    //Con esta ruta aceptamos el pedido en el cual asignamos en un nuevo array nuestra orden aceptada y le agregamos el status de ok 
+    // http://localhost:3000/client/pedidos/accept
+    CLIENT.get('/pedidos/accept', (req,res) => {            
+        if (status[0] === false) {
+            if (pedidos.length > 0) {            
+                status[0] = true;
+                const aceptar = {status, pedidos};
+                accept.push(aceptar);
+                res.json(accept);                                
+            } else {
+                res.send('No tienes ningun pedido');
+            }
+        } else {
+            res.send('Ya tienes un pedido');            
+        }
     });
 
 
